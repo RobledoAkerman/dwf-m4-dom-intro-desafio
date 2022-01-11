3b28ffc0a4b0194307845f02af361d9d0f489440
M. Daniela Robledo Akerman <danielaakerman14@gmail.com>
2022-01-11 11:07:17 -0300

    desafio resuelto

--- index.js
+++ index.js
@@ -25,6 +25,33 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  //eliminar  todos los elementos <li>
+  //de la lista <ul class=”lista”>
+  //(sin eliminar la lista <ul>).
+
+  const listaEls = document.querySelector(".lista");
+  const element = document.querySelectorAll(".lista li");
+
+  for (const el of element) {
+    el.remove();
+  }
+  
+
+  // Crear nuevos elementos <li>
+  // basados en el array cosasQueAprendimos[]
+  // que está en el index.js.
+  // Por cada elemento del array
+  // crear un elemento <li>
+  // con la clase indicada en cada elemento.
+
+  for (const item of cosasQueAprendimos) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = item.tema;
+    newLiEl.classList.add(item.class || "item");
+    listaEls.appendChild(newLiEl);
+    console.log(listaEls);
+  }
+}
 
 main();
